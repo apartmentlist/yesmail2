698a21b23c200df700949a14142e6120515ee52d
alan jones <erem.zeit@gmail.com>
2013-10-22 13:31:17 -0700

    Adding some comments

--- lib/yesmail2/reference_data.rb
+++ lib/yesmail2/reference_data.rb
@@ -4,16 +4,33 @@ module Yesmail2
       'reference-data'
     end
 
+    # Returns a list of datasets in the form:
+    # {"datasetCount"=>3,
+    #   "datasets"=>
+    #    [{"name"=>"DATASET1", "recordCount"=>3160313},
+    #     {"name"=>"DATASET2", "recordCount"=>8023825},
+    #     {"name"=>"DATASET3", "recordCount"=>7341190}]}
     def self.summary
       # GET reference-data
       get(full_path, {})
     end
 
+    # @param dataset_name [String] the name of a dataset as
+    # returned by ::sumary
     def self.dataset_summary(dataset_name)
       # GET reference-data/{dataset}
       get(full_path(dataset_name), :params => {})
     end
 
+    # Query for data in the reference table
+    # @param dataset_name [String] the name of a dataset as
+    # returned by ::sumary
+    # @option limit [Integer] a limit of the number of records to return
+    # @option offset [Integer] an offset at which to start returning records
+    # @option (columnname) [String] in this option, the key is the columnname and
+    #   the value is the value that you want to filter by.  By including
+    #   columnnames in the options list, you are essentially adding]
+    #   a WHERE clause to your query.
     def self.records(dataset_name, options = {})
       # GET reference-data/{dataset}/records
       get(full_path(dataset_name, 'records'), :params => options)
